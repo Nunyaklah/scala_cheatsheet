ed776058624ea2d3a71e4d900e701c9a37e363f2
Elvis020 <spaceyel3@gmail.com>
2021-11-07 21:16:35 +0000

    Added some new changes to the arrays section 😎

--- arrays.scala
+++ arrays.scala
@@ -1,37 +1,60 @@
+/*
+    At the end of this tutorials, you would learn how to:
+        1. Declare arrays in scala
+        2. Populate arrays
+        3. Iterate through an array
+        4. Concatenate arrays
+ */
+
+
+/* Assignment and Declaration */
+
+
 // how to declare an array
 
-val arrayname : Array[Int] = new Array[Int](5)
+//val arrayname_1 : Array[Int] = new Array[Int](5)
 
 // the above means i am declaring an arrray called arrayname , its type being Array[Int] .
 // Then we give it a size by new Array[Int](5), 5 is the size
 
-// second sythax for declaring an array
+// second syntax for declaring an array
 
-val arrayname = new Array[Int](5)
+val arrayName_2 = new Array[Int](5)
 
 //scala arrays index start from 0
 
 //assigning members to array
-arrayname(0) = 10
-arrayname(1) = 30
-arrayname(2) = 20
-arrayname(3) = 58
-arrayname(4) = 23
+arrayName_2(0) = 10
+arrayName_2(1) = 30
+arrayName_2(2) = 20
+arrayName_2(3) = 58
+arrayName_2(4) = 23
 
 //Directly declaring and assigning items to an array
 
-val arrayname = Array(3,4,5,6,7)
+val arrayname_3 = Array(3,4,5,6,7)
 
 
+
+/* Iteration */
 //printing members of array
 // iterates through array and print items
-for (y <- arrayname){
+for (y <- arrayname_3){
     println(y)
 }
+
+// This can also be done using
+arrayname_3.map(println)
 
 //NOTE: Arrays which have not been assinged any memebers take the default value of the data type it was declared with
 
 
+/*   Concatenation
 
-//Concatenation 
-concat(firstarrayname, secondarrayname) // remember the two arrays you are concantenating must be of the same type
+
+    concat(firstarrayname, secondarrayname) // remember the two arrays you are concantenating must be of the same type
+ */
+
+// You can also concatenate arrays using ++ and using the .concat method
+val newConcatArray: Array[Int] = arrayname_3 ++ arrayName_2
+val newConcatArray_2: Array[Int] = arrayname_3.concat(arrayName_2)
